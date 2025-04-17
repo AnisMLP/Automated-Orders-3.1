8fe00439d43c814ef39919b465f813be1ef467d5
AnisMLP <anis@mlperformance.co.uk>
2025-04-17 09:51:42 +0800

    Latest Version 17 April 2025 V2.0

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def add_backup_shipping_note(data):
 
     sku_by_vendor = group_skus_by_vendor(line_items)
     rows_data = [
-        [order_created, order_number, order_id, ', '.join(skus), vendor, order_country, "", "", "", "", "", "", "", backup_note]
+        [order_created, order_number, order_id, ', '.join(skus), vendor, order_country, "", "", "", "", "", "", backup_note, ""]
         for vendor, skus in sku_by_vendor.items()
     ]
 
